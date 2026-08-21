9e1978881140fbf0b241f7110227a466eae60128
jafarkhan83 <jafarkhan.master@gmail.com>
2026-08-22 01:56:04 +0500

    - chat history implementation. - replaced python dict by ChatPromptTemplate of langchain.core.prompts for prompt generation. - implementation of InMemmoryChatMessageHistory that stores message history in memory via python dict named store. - small modifications in json schema for chat history retrieval from response json

--- src/llm.py
+++ src/llm.py
@@ -1,11 +1,16 @@
 from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.chat_history import InMemoryChatMessageHistory
 from dotenv import load_dotenv
 from schemas import json_schema
 
 load_dotenv()
+store = {}
 
 llm = ChatGroq(
     model="qwen/qwen3.6-27b",
+    max_tokens=4096,
     model_kwargs= {
         "response_format": {
             "type": "json_schema",
@@ -17,18 +22,30 @@ llm = ChatGroq(
     }
     )
 
-def create_response(prompt):
-    messages = [
-        (
-            "system",
-            "You are a helpful assistant."
-        ),
-        (
-            "user", prompt
-        )
-    ]
-    return messages
+def create_prompt():
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "You are a helpful assistant."),
+        MessagesPlaceholder(variable_name="history"),
+        ("user", "{query}")
+        ])
+    return prompt
 
-def get_response(prompt):
-    response = create_response(prompt)
-    return llm.invoke(response)
+def get_session(session_id):
+    if session_id not in store:
+        store[session_id] = InMemoryChatMessageHistory()
+    return store[session_id]
+
+def get_response(query):
+    prompt = create_prompt()
+    chain = prompt | llm
+
+    chain_with_history = RunnableWithMessageHistory(
+        chain,
+        get_session,
+        input_messages_key="query",
+        history_messages_key="history"
+    )
+
+    return chain_with_history.invoke(
+        {"query": query},
+        config={"configurable" : {"session_id" : "session_1"}})
--- src/schemas.py
+++ src/schemas.py
@@ -10,9 +10,18 @@ json_schema = {
                 "input_user_tokens": {"type": "integer"},
                 "input_system_tokens": {"type": "integer"},
                 "output_tokens": {"type": "integer"},
-                "total_tokens": {"type": "integer"}
+                "total_tokens": {"type": "integer"},
+                "chat_history": {
+                    "type": "array", 
+                    "items": {"type": "object",
+                                "properties": {
+                                    "user_asked": {"type": "string"},
+                                    "answer": {"type": "string"}
+                                    },
+                                    "required": ["user_asked", "answer"]
+                                }}
             },
-            "required": ["input_user_tokens", "input_system_tokens", "output_tokens", "total_tokens"]
+            "required": ["input_user_tokens", "input_system_tokens", "output_tokens", "total_tokens", "chat_history"]
         }
     },
     "required": ["title", "description", "content", "metadata"]
